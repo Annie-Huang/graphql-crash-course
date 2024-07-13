13cb31969880efbd3d66b87c7e32912285bdbc76
Annie Huang <annie.hn@gmail.com>
2024-07-13 14:26:19 +1000

    Create ApolloServer and startStandaloneServer in index.ts

--- index.ts
+++ index.ts
@@ -20,3 +20,19 @@ const typeDefs = `#graphql
     books: [Book]
   }
 `;
+
+// The ApolloServer constructor requires two parameters: your schema
+// definition and your set of resolvers.
+//
+// sever setup
+const server = new ApolloServer({});
+
+// Passing an ApolloServer instance to the `startStandaloneServer` function:
+//  1. creates an Express app
+//  2. installs your ApolloServer instance as middleware
+//  3. prepares your app to handle incoming requests
+const { url } = await startStandaloneServer(server, {
+  listen: { port: 4000 },
+});
+
+console.log(`Server ready at port`, 4000);
